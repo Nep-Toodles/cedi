b82c3290e68ce90f663e74923b2cff0a730abfe0
Nep Toodles <59413955+Nep-Toodles@users.noreply.github.com>
2021-01-23 19:38:51 +0545

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-var app = express()
-const fs = require("fs")
-const path = require("path")
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname , "src/index.html"))
-})
-app.listen(3000) 
-app.use(express.static("src"))
