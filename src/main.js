2db92424eec66ac2fd208a2d14d2cd24400fe790
Nep Toodles <ganukarki7@gmail.com>
2021-01-23 10:20:20 +0000

    PWA 2nd Phase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 window.addEventListener("scroll",()=>{
   window.scrollTo(0,0)
 })
+window.addEventListener('appinstalled', (e) => {
+  $.notify("Sucessfully installed!!!","Success")
+});
 $(document).ready(() => {
   var html = localStorage.getItem("html") ? localStorage.getItem("html") : localStorage.setItem("html",``)
   var css = localStorage.getItem("css") ? localStorage.getItem("css") : localStorage.setItem("css",``)
@@ -417,3 +420,29 @@ function preventBehavior(e) {
 };
 document.addEventListener("touchmove", preventBehavior, { passive: false });
 window.oncontextmenu = (e) => e.preventDefault()
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    addBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
